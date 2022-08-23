50b0d376ed3da5d3bd40df3c801accd5c00ac58d
Nestor Mamani <nmamanipantoja@gmail.com>
2022-08-23 16:49:27 -0500

    🧪 Test CI

--- src/ExampleComponent.tsx
+++ src/ExampleComponent.tsx
@@ -6,7 +6,7 @@ export interface IThingProps {
 
 export const Thing: React.FC<IThingProps> = ({ children }) => {
   return (
-    <div className="bg-blue-300 text-blue-400 p-4 rounded-lg">
+    <div className="bg-indigo-300 text-indigo-600 p-4 rounded-lg">
       {children || `the snozzberries taste like snozzberries`}
     </div>
   )
